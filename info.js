2d57ae73f2cc82d51f056fc45675eecaaf27bc13
Molary <molary23@gmail.com>
2022-06-02 13:34:43 +0100

    the start of the project and creation of json file

---
+++ info.js
@@ -0,0 +1,18 @@
+const info = {
+  education: {
+    noun: {
+      name: "national open university",
+      course: "information technology",
+      degree: ["masters", "msc"],
+      from: "2016",
+      to: "2019",
+    },
+    oou: {
+      name: "olabisi onabanjo university",
+      course: "computer science",
+      degree: ["bachelor", "bsc"],
+      from: "2005",
+      to: "2010",
+    },
+  },
+};
