132d79f42186413a23433621c2d4221c982201bb
Molary <molary23@gmail.com>
2022-06-15 14:07:31 +0100

    second upload

--- src/services/info.request.js
+++ src/services/info.request.js
@@ -2,7 +2,7 @@ export const pullInfo = async () => {
   const URL = "https://www.hassanadeola.com/outbox/info.json";
   try {
     const requestOptions = {
-      method: "GET",
+      method: "POST",
       headers: { "Content-Type": "application/json" },
       body: process.env.REACT_APP_SECURE_INFO_KEY,
     };
